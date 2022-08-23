b890a428dc666e3bf91257649a035959bef5d6d7
kashif s <kashee@softpyramid.com>
2022-08-23 16:07:25 +0500

    final and last

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Button from './Button.jsx';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "https://pokeapi.co/api/v2/pokemon?limit=50",
+    }).then(function (res) {
+      setData(res?.data?.results);
+    });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+      <div
+        style={{
+          marginTop: "40px",
+          justifyContent: "space-around",
+          display: "flex",
+          flexWrap: "wrap",
+          width: "90%",
+          margin: "auto",
+        }}
+      >
+        {data?.map((poke, i) => {
+          return (
+            <div
+              key={i}
+              style={{
+                width: "400px",
+                height: "330px",
+                border: "2px solid #000000",
+                margin: "30px 10px",
+              }}
+            >
+              <div style={{ padding: "5px 10px" }}>
+                <p style={{ fontWeight: "bold", textTransform: "capitalize" }}>
+                  {" "}
+                  {poke.name}
+                </p>
+              </div>
+              <img
+                style={{ height: "300px", width: "300px" }}
+                alt="pokemon"
+                src={`https://img.pokemondb.net/artwork/large/${poke.name}.jpg`}
+              />
+            </div>
+          );
+        })}
+        <div></div>
+      </div>
+    </>
   );
 }
 
